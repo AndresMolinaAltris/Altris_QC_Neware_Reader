9c50e054b16a93213dd6d321b80f611972ec7ee4
Andres Molina <andres.molina@altris.se>
2025-06-04 09:16:19 +0200

    Added DQDVAnalysis to project imports

--- common/project_imports.py
+++ common/project_imports.py
@@ -3,7 +3,7 @@
 Internal project imports to avoid circular dependencies.
 """
 # Import this instead of directly importing project modules
-from features import Features
+from features import Features, DQDVAnalysis
 from cell_database import CellDatabase, find_active_mass
 from data_import import extract_active_mass, extract_cell_id, extract_sample_name
 from neware_plotter import NewarePlotter
